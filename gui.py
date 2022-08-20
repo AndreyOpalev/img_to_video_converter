a34398745eadbfe44c8cbf2f089701e28966efd8
Andrey Opalev <andrey.o.opalev@yandex.ru>
2022-08-20 14:43:27 +0300

    Created a simple GUI with Tkinter and adjusted to it the conversion script

---
+++ gui.py
@@ -0,0 +1,131 @@
+import sys
+import os
+from tkinter import Tk, Button, Frame, filedialog, Label
+from tkinter import ttk
+import tkinter
+from tkinter.scrolledtext import ScrolledText
+import img_to_video as converter
+import threading
+
+class StdoutRedirector(object):
+
+    # Pass reference to the text widget
+    def __init__(self, text_widget):
+        self.text_space = text_widget
+
+    # Write text and scroll to end
+    def write(self,string):
+        self.text_space.insert('end', string)
+        self.text_space.see('end')
+
+        # force refresh of the widget to be sure that thing are displayed
+        # self.text_space.update_idletasks()
+        
+    # Needed for file like object
+    def flush(self):
+        pass
+        # force refresh of the widget to be sure that thing are displayed
+        # self.text_space.update_idletasks()
+
+class Observer(object):
+
+    def __init__(self, main_gui):
+        self.gui = main_gui
+
+    def update(self, value):
+        pass
+        #percentage = int((value / self.max_val) * 100.0)
+        #self.pbar['value'] += percentage
+        #self.pbar.update_idletasks()
+
+    def done(self):
+        print("Done!")
+        self.gui.enable()
+
+    def set_max(self, max_value):
+        pass
+        #self.max_val = max_value
+
+# TODO: Destroy the thread if it's closed
+class MainGUI(Frame):
+
+    def __init__(self, root=None):
+        self.create_widgets(root)
+
+        self.observer = Observer(self)
+
+    def create_widgets(self, root):
+        self.frm = ttk.Frame(root, padding = 10)
+        self.frm.grid()
+        
+        # self.frm.columnconfigure(0, weight=1)
+        self.frm.columnconfigure(1, weight=1)
+        
+        folder_button = ttk.Button(self.frm, text="Fodler...", command=self.select_folder)
+        folder_button.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+        
+        self.folder_label = ttk.Label(self.frm, text="/Users/andlev/Workspace/ImageDropletsToVideo/1min")
+        self.folder_label.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
+
+        total_image_width = ttk.Label(self.frm, text="Image width, mm:")
+        total_image_width.grid(row=1, column=0, sticky="w", padx=5, pady=5)
+        self.image_width_entry = ttk.Entry(self.frm)
+        self.image_width_entry.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
+
+        capture_interval_label = ttk.Label(self.frm, text="Capture interval, s:")
+        capture_interval_label.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+        self.capture_interval_entry = ttk.Entry(self.frm)
+        self.capture_interval_entry.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
+
+        video_time_label = ttk.Label(self.frm, text="Video time (approx.), s:")
+        video_time_label.grid(row=3, column=0, sticky="w", padx=5, pady=5)
+        self.video_time_entry = ttk.Entry(self.frm)
+        self.video_time_entry.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
+
+        #self.pbar = ttk.Progressbar(self.frm, orient=tkinter.HORIZONTAL, length = 100, mode='determinate')
+        #self.pbar.grid(row=4, column=0, columnspan=2, sticky="nsew")
+        #self.pbar_updater = TkProgressBarUpdater(self.pbar)
+
+        self.generate_button = ttk.Button(self.frm, text="Generate", command=self.start_generation_in_thread)
+        self.generate_button.grid(row=5, column=1, sticky="e")
+        
+        self.log_widget = ScrolledText(self.frm, font=("consolas", "12", "normal"))
+        self.log_widget.grid(row=6, column=0, columnspan=2, sticky="nsew")
+        sys.stdout = StdoutRedirector(self.log_widget)
+
+    def enable(self):
+        self.generate_button["state"] = "normal"
+        
+    def select_folder(self):
+        directory = filedialog.askdirectory(initialdir = "./")
+        self.folder_label.configure(text = directory)
+
+    def reset_logging(self):
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+    def start_generation_in_thread(self):
+        self.generate_button["state"] = "disabled"
+        threaded = threading.Thread(target=self.start_generation)
+        threaded.start()
+
+    def start_generation(self):
+        folder_path = self.folder_label.cget("text")
+        #image_width_mm = float(self.image_width_entry.get())
+        #capture_interval_s = int(self.capture_interval_entry.get())
+
+        image_width_mm = 4.7
+        capture_interval_s = 10
+        
+        converter.generate_img_to_video(folder_path,
+                                        image_width_mm,
+                                        capture_interval_s,
+                                        self.observer)
+        
+if __name__ == "__main__":
+    root = Tk();
+    root.wm_title("Images to Video conversion")
+    app = MainGUI(root)
+    root.resizable(False, False)
+    root.mainloop()
+
--- img_to_video.py
+++ img_to_video.py
@@ -2,6 +2,7 @@ import cv2
 import sys, glob, os
 import tqdm
 import subprocess
+import time
 
 class Time:
     def __init__(self):
@@ -17,30 +18,38 @@ class Time:
 
         return "{:02}:{:02}:{:02}".format(h, m, s)
 
-def main(folder, los_width, frame_interval_s):
+def generate_img_to_video(folder, pixels_per_cm, frame_interval_s, observer = None):
     print("Folder to process: {}.".format(folder))
-    print("Width of the image, mm: {}.".format(los_width))
-    print("Time interval: {} seconds".format(frame_interval_s))
+    print("Width of the image, mm: {}.".format(pixels_per_cm))
+    print("Time interva, seconds: {}.".format(frame_interval_s))
 
     files = glob.glob(folder + '/*.JPG')
     files.sort()
-
-    # Calculate the width of a scale bar
+    
+    # Get image resolution
     img = cv2.imread((files[0]))
     height, width, layers = img.shape
-    scale_bar_width = round(width / los_width) # num. of pixel that 1 mm takes
     print("Image size: {}x{}.".format(width, height))
+    
+    # Calculate desired video resolution    
+    dwidth = 1024
+    scale_ration = dwidth / width
+    dheight = int(height * scale_ration)
+
+    # TODO: Consider different length of scale bars    
+    # Calculate the width of a scale bar
+    scale_bar_width = round(dwidth / pixels_per_cm) # num. of pixel that 1 mm takes    
     print("1 mm takes {} pixels.".format(scale_bar_width))
 
     # Margins for drawings
     h_margin = 20
-    v_margin = 100
-    scal_bar_heigh = 10
+    v_margin = 50
+    scale_bar_height = 2
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_size = 48
-    thickness = 2
-    font_scale = 1
+    font_size = 10 # 48
+    thickness = 1
+    fontscale = 1
     color = (255, 255, 255)
     color_bg = (0, 0, 0)
 
@@ -52,47 +61,77 @@ def main(folder, los_width, frame_interval_s):
 
     index = 0
 
-    with tqdm.tqdm(total = len(files)) as pbar:
-        for file_name in files:
-            img = cv2.imread(file_name)
+    pbar = None
+    if observer is None:
+        pbar = tqdm.tqdm(total = len(files))
+    
+    # TODO: Parallelize it. Timestamps can-be computed before, as
+    # well as text positions
+    for file_name in files:
+        print("Proccessing: {}.".format(file_name));
+        
+        img = cv2.imread(file_name)
+        img = cv2.resize(img, (dwidth, dheight), interpolation = cv2.INTER_AREA)
 
-            # Draw the scale bar.
-            # pt1 and pt2 can be calculated outside
-            pt1 = (width - scale_bar_width - h_margin, height - v_margin + scal_bar_heigh)
-            pt2 = (width - h_margin, height - v_margin)
-            img = cv2.rectangle(img, pt1, pt2, color, cv2.FILLED)
+        # Draw the scale bar.
+        # pt1 and pt2 can be calculated outside
+        pt1 = (dwidth - scale_bar_width - h_margin,
+               dheight - v_margin + scale_bar_height)
+        pt2 = (pt1[0] + scale_bar_width,
+               pt1[1] - scale_bar_height)
+        # dwidth - h_margin, dheight - v_margin)
+        img = cv2.rectangle(img, pt1, pt2, color, cv2.FILLED)
 
-            # Draw the unit text below the bar
-            org = (width - int(scale_bar_width / 2) - h_margin, height - v_margin - scal_bar_heigh + font_size + 10)
-            img = cv2.putText(img, '1 mm', org, font, font_scale, color, thickness, cv2.LINE_AA)
+        # Draw the unit text below the bar
+        text_size, _ = cv2.getTextSize('1 mm', font, fontscale * 0.5, 1)
+        text_w, text_h = text_size
 
-            # Draw the time
-            org = (0 + h_margin, 0 + v_margin)
-            x, y = org
-            text_size, _ = cv2.getTextSize(time_stamp.to_str(), font, 2, thickness)
-            text_w, text_h = text_size
-            img = cv2.rectangle(img, org, (x + text_w, y -  text_h), color_bg, cv2.FILLED)
-            img = cv2.putText(img, time_stamp.to_str(), org, font, 2, color, thickness, cv2.LINE_AA)
+        org = (dwidth - int(scale_bar_width / 2) - int(text_w / 2) - h_margin,
+               pt1[1] + scale_bar_height + text_h)
+        
+        img = cv2.putText(img, '1 mm', org, font, fontscale * 0.5, color, 1, cv2.LINE_AA)
 
-            cv2.imwrite("tmp/{}.JPG".format(index), img)
-            index += 1
+        # Draw the time
+        org = (0 + h_margin, 0 + v_margin)
+        x, y = org
+        text_size, _ = cv2.getTextSize(time_stamp.to_str(), font, fontscale, thickness)
+        text_w, text_h = text_size
+        img = cv2.rectangle(img, org, (x + text_w, y - text_h), color_bg, cv2.FILLED)
+        img = cv2.putText(img, time_stamp.to_str(), org, font, fontscale, color, thickness, cv2.LINE_AA)
 
-            time_stamp.increment(frame_interval_s)
+        cv2.imwrite("tmp/{}.JPG".format(index), img)
+        index += 1
 
-            # Updated status bar
+        time_stamp.increment(frame_interval_s)
+
+        # Updated status bar
+        if pbar is not None:
             pbar.update(1)
     
     # Run ffmpeg to generate video
-    subprocess.call(['ffmpeg', '-r', '24', '-i', 'tmp/%d.JPG', 'out.mp4'])
+    # TODO: Calculate the output frame rate based on the required video length
+    process = subprocess.Popen(['ffmpeg', '-y', '-r', '24', '-i', 'tmp/%d.JPG', 'out.mp4'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+
+    while True:
+        for line in process.stdout:
+            print(line.rstrip())
+            
+        if  process.poll() is not None:
+            break
+        
+        time.sleep(1)
 
     # Delete all images
     for file in glob.glob('tmp/*'):
         os.remove(file)
     os.rmdir('tmp')
 
+    # Notify it's done
+    observer.done()
+
 if __name__ == '__main__':
     path = sys.argv[1]
-    width = float(sys.argv[2])
+    dwidth = float(sys.argv[2])
     frame_interval = int(sys.argv[3])
 
-    sys.exit(main(path, width, frame_interval))
+    sys.exit(generate_img_to_video(path, dwidth, frame_interval))
